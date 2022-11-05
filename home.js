d951f75404eda614c6ececd74706d415f79f6c2a
srucha20 <rucha.gsaraf@gmail.com>
2022-11-05 14:22:20 +0000

    initial files push

---
+++ home.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    alert('Hi')
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
