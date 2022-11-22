ff3774f84ba2e4870245d587272a8d99bac794df
srucha20 <rucha.gsaraf@gmail.com>
2022-11-22 16:34:44 +0000

    removed unused code

--- home.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    alert('Hi')
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
